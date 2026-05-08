3472198f4ed2906104dbfa9c2c47476df582e4ba
Spike <2435850271@qq.com>
2026-05-08 02:26:58 -0700

    Define packet header size explicitly

--- shared/include/protocol/PacketHeader.h
+++ shared/include/protocol/PacketHeader.h
@@ -5,8 +5,6 @@
 
 #include "protocol/Command.h"
 
-#pragma pack(push, 1)
-
 struct PacketHeader {
     uint16_t signature;
     uint32_t length;
@@ -14,8 +12,16 @@ struct PacketHeader {
     uint8_t md5_hash[16];
 };
 
-#pragma pack(pop)
+constexpr size_t PACKET_SIGNATURE_SIZE = sizeof(uint16_t);
+constexpr size_t PACKET_LENGTH_SIZE = sizeof(uint32_t);
+constexpr size_t PACKET_COMMAND_SIZE = sizeof(uint16_t);
+constexpr size_t PACKET_MD5_SIZE = 16;
 
-constexpr size_t PACKET_HEADER_SIZE = sizeof(PacketHeader);
+constexpr size_t PACKET_HEADER_SIZE =
+    PACKET_SIGNATURE_SIZE +
+    PACKET_LENGTH_SIZE +
+    PACKET_COMMAND_SIZE +
+    PACKET_MD5_SIZE;
+
 constexpr uint16_t PACKET_SIGNATURE = 0xABCD;
 constexpr uint32_t MAX_BODY_SIZE = 1048576;
